3794543f4c9e32b9ed8a8b4cea4d7f957da5a4c4
dhakerbh <dhakerbh@gmail.com>
2024-02-27 11:15:08 +0100

    added Header

---
+++ src/app/header.tsx
@@ -0,0 +1,42 @@
+"use client";
+import "./header.css";
+import { FaYoutube, FaRegFilePdf, FaImage } from "react-icons/fa";
+import { GrDocumentTxt } from "react-icons/gr";
+import { MdInfoOutline } from "react-icons/md";
+import { BsFiletypeTxt } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="logo.png" alt="logo" />
+
+      <li>
+        {" "}
+        <FaYoutube className="react-icons" />
+        <a href="mod1">YouTube Video Summarizer</a>
+      </li>
+      <li>
+        {" "}
+        <FaRegFilePdf className="react-icons" />
+        <a href="mod2">PDF Summarizer</a>
+      </li>
+      <li>
+        {" "}
+        <FaImage className="react-icons" />
+        <a href="mod3">Image To Text</a>
+      </li>
+      <li>
+        {" "}
+        <BsFiletypeTxt className="react-icons" />
+        <a href="mod4">Text Summarizer</a>
+      </li>
+      <li>
+        {" "}
+        <MdInfoOutline className="react-icons" />
+        <a href="about">About Us</a>
+      </li>
+    </header>
+  );
+};
+
+export default Header;
