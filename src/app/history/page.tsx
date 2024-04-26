1343cf7e1abd47551f7068b7553cca752f10eb50
dhakerbh <dhakerbh@gmail.com>
2024-04-26 23:19:30 +0100

    working history section

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 import { useEffect, useState } from "react";
+import { MdInfoOutline } from "react-icons/md";
+import { BsFiletypeTxt } from "react-icons/bs";
+import { FaYoutube, FaRegFilePdf, FaImage } from "react-icons/fa";
 
+import "./history.css";
 const history = () => {
   const [historyElements, setHistoryElements] = useState<Array<string>>([]);
-  const [showMore, setShowMore] = useState(false);
-
+  const [showMoreList, setShowMoreList] = useState<Array<boolean>>([]);
   useEffect(() => {
     getHistory();
   }, []);
   const email = localStorage.getItem("email");
-
+  function getIcon(module: any) {
+    switch (module) {
+      case "Youtube Summarizer":
+        return <FaYoutube className="react-icons" />;
+      case "PDF Summarizer":
+        return <FaRegFilePdf className="react-icons" />;
+      case "Image To Text":
+        return <FaImage className="react-icons" />;
+      default:
+        return <BsFiletypeTxt className="react-icons" />;
+    }
+  }
   async function getHistory() {
     const email = localStorage.getItem("email");
     await fetch("http://127.0.0.1:8080/history", {
@@ -20,7 +34,11 @@ const history = () => {
       body: JSON.stringify({ email }),
     })
       .then((data) => data.json())
-      .then((data) => setHistoryElements(data["result"]));
+      .then((data) => {
+        setHistoryElements(data["result"]);
+        let arr = new Array(data["result"].length).fill(false);
+        setShowMoreList(arr);
+      });
   }
   return (
     <div className="hs-container">
@@ -28,27 +46,38 @@ const history = () => {
         <div className="nohistory">No History yet , Create One </div>
       )}
       {historyElements &&
-        historyElements.map((line: any) => {
+        historyElements.map((line: any, id: any) => {
           return (
-            <div key={line}>
-              <div className="module">{line.module}</div>
+            <div className="line" key={id}>
+              <div className="module">
+                <span className="icon">{getIcon(line.module)}</span>
+              </div>
               <div className="details">
                 <div className="about">
-                  <span id="link">{line.link}</span>
-                  <span id="time">{line.time}</span>
+                  <p id="link">{line.link}</p>
+                  <p id="time">{line.time}</p>
                 </div>
                 <div className="content">
-                  <h6>
-                    {showMore
+                  <h4>
+                    {showMoreList[id]
                       ? line.data
                       : `${line.data.toString().substring(0, 255)}...`}
+
                     <button
                       className="btn"
-                      onClick={() => setShowMore(!showMore)}
+                      onClick={() => {
+                        let arr = showMoreList;
+                        setShowMoreList(
+                          showMoreList.map((item, index) =>
+                            index == id ? !item : item
+                          )
+                        );
+                        console.log(showMoreList[id]);
+                      }}
                     >
-                      {showMore ? "Show less" : "Show more"}
+                      {showMoreList[id] ? "Show less" : "Show more"}
                     </button>
-                  </h6>
+                  </h4>
                 </div>
               </div>
               {
